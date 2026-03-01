09f10b4f7a2bf9727db05fd28e74f850fe110229
Mike Monaco <mamonaco1973@gmail.com>
2026-03-01 16:43:44 -0500

    2026 tech refresh

--- 01-sb/functions.tf
+++ 01-sb/functions.tf
@@ -156,16 +156,13 @@ resource "azurerm_function_app_flex_consumption" "keygen_func" {
   app_settings = {
     # Flex rejects FUNCTIONS_WORKER_RUNTIME. Do not set it here.
     FUNCTIONS_EXTENSION_VERSION = "~4"
-
     APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.func_ai.connection_string
 
     # ----------------------------
     # Service Bus (RBAC)
     # ----------------------------
     SERVICEBUS_QUEUE_NAME = azurerm_servicebus_queue.keygen_queue.name
-
     SERVICEBUS_NAMESPACE_FQDN = "${azurerm_servicebus_namespace.keygen_ns.name}.servicebus.windows.net"
-
     ServiceBusConnection__fullyQualifiedNamespace = "${azurerm_servicebus_namespace.keygen_ns.name}.servicebus.windows.net"
 
     # ----------------------------
